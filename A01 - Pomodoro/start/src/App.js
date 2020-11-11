4d4fad9e0665fe3997ef2d7cbba76d17194e99d0
doylerules85 <rmdoyledesign@gmail.com>
2020-11-10 21:48:41 -0700

    App 1 & 2

--- A01 - Pomodoro/start/src/App.js
+++ A01 - Pomodoro/start/src/App.js
@@ -1,21 +1,70 @@
 import React from 'react';
 import './App.css';
 
+function padTime(time){
+return time.toString().padStart(2,'0');
+}
+
 export default function App() {
+
+  const [title, setTitle] = React.useState('Let the countdown begin!');
+  const [timeLeft, setTimeLeft] = React.useState(25 * 60);
+  const [isRunning, setIsRunning] = React.useState(false);
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(Math.floor(timeLeft - (minutes * 60)));
+  let intervalRef = React.useRef(null);
+
+  function startTimer(){
+    if(intervalRef.current !== null){
+      return;
+    }
+    setTitle('And away we go!');
+    setIsRunning(true);
+    intervalRef.current = setInterval(() =>{
+      setTimeLeft(timeLeft =>{
+        if(timeLeft >= 1){
+          return timeLeft - 1;
+        }
+        return 0;
+      });
+    },1000);
+    console.log(intervalRef.current)
+  }
+
+  function resetTimer(){
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Anotha One?!');
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  }
+
+  function stopTimer(){
+    if(intervalRef.current === null){
+      return;
+    }
+    console.log(intervalRef.current);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('keep it going!');
+    setIsRunning(false);
+  }
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{ minutes }</span>
         <span>:</span>
-        <span>00</span>
+        <span>{ seconds }</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        { !isRunning && <button onClick={startTimer}>Start</button>}
+        { isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
--- A02 - Markdown Editor/start/src/App.js
+++ A02 - Markdown Editor/start/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 export default function App() {
+
+  const [markdown, setMarkDown] = useState('# supp');
+
+  function handleChange(e){
+    setMarkDown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea onChange={handleChange} value={markdown}/>
+      <ReactMarkdown className="preview" children={markdown}></ReactMarkdown>
     </div>
   );
 }
