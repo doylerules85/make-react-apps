acb6f335ffa63a07e7b3262d078fa8d3335f2813
doylerules85 <rmdoyledesign@gmail.com>
2020-11-12 08:53:48 -0700

    Part 2 - Adding Link-Refs // Custom useMovement Hook

--- A05 - Moving Link/start/src/App.js
+++ A05 - Moving Link/start/src/App.js
@@ -1,11 +1,14 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
+import useMovement from './useMovement';
 
 export default function App() {
   const canvasRef = useRef(null);
   const linkDownRef = useRef(null);
-  const [x,setX] = useState(0);
-  const [y,setY] = useState(0);
+  const linkUpRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const {x,y,direction,move} = useMovement();
 
   // set height of canvas
   useEffect(() =>{
@@ -13,54 +16,38 @@ export default function App() {
     // setting canvas to window W&H
     context.canvas.height = window.innerHeight;
     context.canvas.width = window.innerWidth;
-    
   }, []);
   
   // move the box if x or y changes
   useEffect(() =>{
     const context = canvasRef.current.getContext('2d');
     context.clearRect(0, 0, window.innerWidth, window.innerHeight )
-    context.drawImage(linkDownRef.current, x, y);
+
+    let theLinkRef;
+    if(direction === 'down') theLinkRef = linkDownRef;
+    if(direction === 'up') theLinkRef = linkUpRef;
+    if(direction === 'right') theLinkRef = linkRightRef;
+    if(direction === 'left') theLinkRef = linkLeftRef;
+
+    context.drawImage(theLinkRef.current, x, y);
   }, [x,y]);
-
-  // event listener to window to listen to window keys
-  useEffect(() =>{
-
-    window.addEventListener('keydown', handleKeyDown);
-
-    function handleKeyDown(e){
-      if(e.key === 'ArrowUp') move('up');
-      if(e.key === 'ArrowLeft') move('left');
-      if(e.key === 'ArrowRight') move('right');
-      if(e.key === 'ArrowDown') move('down');
-    }
-
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, []);
-
-  function move(direction){
-      if(direction === 'up') setY((y) => y - 20);
-      if(direction === 'left') setX((x) => x - 20);
-      if(direction === 'right') setX((x) => x + 20);
-      if(direction === 'down') setY((y) => y + 20);
-  }
 
   return (
     <div className="app">
       <canvas ref={canvasRef}/>
 
       <div className="arrows">
-        <button onClick={() => move('up')}>Left</button>
-        <button onClick={() => move('left')}>Up</button>
+        <button onClick={() => move('up')}>Up</button>
+        <button onClick={() => move('left')}>Left</button>
         <button onClick={() => move('down')}>Down</button>
         <button onClick={() => move('right')}>Right</button> 
       </div>
 
       <div className="images">
         <img ref={linkDownRef} src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+        <img ref={linkRightRef} src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img ref={linkLeftRef} src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
       </div>
     </div>
   );
