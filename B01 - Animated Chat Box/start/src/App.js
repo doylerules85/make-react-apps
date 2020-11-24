f9fc7f8feeadd034242af3800a3c9ed9fae0a839
doylerules85 <rmdoyledesign@gmail.com>
2020-11-23 23:14:41 -0700

    animated chat box - Start & Finish

--- B01 - Animated Chat Box/start/src/App.js
+++ B01 - Animated Chat Box/start/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import useInterval from '@use-it/interval';
+import {motion} from 'framer-motion';
 import './App.css';
 
 const messages = [
@@ -12,17 +14,57 @@ const messages = [
 ];
 
 export default function App() {
+
+  const [messageToShow, setMessageToShow] = useState(0);
+
+  useInterval(() =>{
+    setMessageToShow(messageToShow => messageToShow + 1);
+  },2000);
+
   return (
     <div className="app">
       <div className="walkthrough">
         {messages.map((message, index) => {
+          const even = index % 2 === 0;
+
+          // when we show the type indicator
+          if(messageToShow + 1 === index){
+            return <TypeIndicator  key={index} even={even}/>
+          }
+
+          // when we show the message
+          if( index > messageToShow) return <div key={index}/>
+
           return (
-            <div key={index} className="message">
-              {message.text}
-            </div>
+            <Message key={index} message={message}/>
           );
+
         })}
       </div>
     </div>
   );
+}
+
+function TypeIndicator({even}){
+  return (
+    <motion.div style={{width: '100%'}} initial={{rotate: 10, scale: 0}} animate={{rotate: 0, scale: 1}}>
+      <div className={`typing ${even ? 'is-right' : 'is-left'}`}>
+        <div className="dots">
+          <div/>
+          <div/>
+          <div/>
+        </div>
+      </div>
+    </motion.div>
+  )
+}
+
+function Message({message}) { 
+  return (
+    <motion.div className="message" initial={{scale: 0.8, y: 100}} animate={{scale: 1, y: 0}}>
+      <div className="avatar">🦋</div>
+      <motion.div className="text" whileHover={{scale: 1.05}}>{message.text}</motion.div>
+      <div className="avatar">🐛</div>
+    </motion.div>
+  )
 }
